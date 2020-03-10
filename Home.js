4e28f77bbe667130ac097430d6ae45bd0a67bcd0
Dan McBride <mcbride.danny@gmail.com>
2020-03-09 22:42:12 -0600

    did it work

--- Home.js
+++ Home.js
@@ -1,7 +1,7 @@
 
 function myFunction() {
     document.getElementById("answer").style.color = "green";
-    document.getElementById("result").innerHTML = "Alex";
+    document.getElementById("result").innerHTML = "Dan";
    
 
 }
