5650a6b156c13f34c942ce6b44520565a2e4b9db
Dan McBride <mcbride.danny@gmail.com>
2020-03-09 22:24:08 -0600

    test

--- Home.js
+++ Home.js
@@ -1,5 +1,7 @@
 
 function myFunction() {
-    document.getElementById("answer").style.color = "blue";
-    document.getElementById("result").innerHTML = "Congrats"
+    document.getElementById("answer").style.color = "green";
+    document.getElementById("result").innerHTML = "At yo momma's house";
+    document.getElementById("result").style.color = "white";
+
 }
