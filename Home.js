1d73becfba03efe7e57833af2cabd235d6c92fbf
Alex McBride <mcbralex@gmail.com>
2020-03-09 21:37:50 -0700

    finalTest

--- Home.js
+++ Home.js
@@ -1,7 +1,7 @@
 
 function myFunction() {
     document.getElementById("answer").style.color = "green";
-    document.getElementById("result").innerHTML = "At yo momma's house";
-    document.getElementById("result").style.color = "white";
+    document.getElementById("result").innerHTML = "Alex";
+   
 
 }
