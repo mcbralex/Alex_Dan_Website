0bd4a4223b143829babe8d459fb80bb5c1f36651
Alex McBride <mcbralex@gmail.com>
2020-03-10 19:36:02 -0700

    First 2 sections html and javascript

--- Home.js
+++ Home.js
@@ -1,7 +1,34 @@
 
-function myFunction() {
-    document.getElementById("answer").style.color = "green";
-    document.getElementById("result").innerHTML = "Dan";
-   
 
+function listFood(){
+    
+    var foods = [
+        "TacoBell",
+        "Del Taco",
+        "McDonalds",
+        "Arby's",
+        "Burger King",
+        "Jack In The Box",
+        "Culver's",
+        "Wendy's"
+
+    ];
+
+    for (i = 0; i <= foods.length; i++){
+        document.getElementById("food"+i).innerHTML = foods[i].toString();
+
+    }
+    
+    
 }
+
+$(document).ready(function() {
+    $(".foodChoices").hide();
+
+    $("#userSubmit").click(function(){
+        $(".users").slideUp();
+        $(".foodChoices").show();
+
+    })
+});
+
