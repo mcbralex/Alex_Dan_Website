a3fcbce25d941e57560954a944d1c3454a8add3c
Dan McBride <mcbride.danny@gmail.com>
2020-03-12 15:01:53 -0600

    second user name

--- Home.js
+++ Home.js
@@ -4,7 +4,11 @@ function listFood(){
     
     var user1 = document.getElementById("user1").value;
 
-    document.getElementById("user1Choose").innerHTML = user1 + " ,Enter 3 Choices";
+    document.getElementById("user1Choose").innerHTML = user1 + ", enter 3 restaurants.";
+
+    var user2 = document.getElementById("user2").value;
+
+    document.getElementById("user2Choose").innerHTML = user2 + ", enter 3 restaurants.";
 
     var foods = [
         "TacoBell",
