b984ffa1193d78dfc23e0dc5af0080eb7c156537
Dan McBride <mcbride.danny@gmail.com>
2020-03-16 21:59:45 -0600

    food mixing

--- Home.js
+++ Home.js
@@ -1,4 +1,4 @@
-
+var foods = [];
 
 function listFood(){
     
@@ -10,25 +10,48 @@ function listFood(){
 
     document.getElementById("user2Choose").innerHTML = user2 + ", enter 3 restaurants.";
 
-    var foods = [
-        "TacoBell",
-        "Del Taco",
-        "McDonalds",
-        "Arby's",
-        "Burger King",
-        "Jack In The Box",
-        "Culver's",
-        "Wendy's"
 
-    ];
 
-    for (i = 0; i <= foods.length; i++){
-        document.getElementById("food"+i).innerHTML = foods[i].toString();
-
-    }
-    
-    
 }
+
+function mixFood() {
+   
+    foods.push(document.getElementById("entry1").value);
+    foods.push(document.getElementById("entry2").value);
+    foods.push(document.getElementById("entry3").value);
+    foods.push(document.getElementById("entry4").value);
+    foods.push(document.getElementById("entry5").value);
+    foods.push(document.getElementById("entry6").value);
+
+    displayFood();
+}
+
+function displayFood() {
+    for (i = 0; i < foods.length; i++){
+        document.getElementById("foods"+i).innerHTML = foods[i];
+    }
+}
+
+function subtractFood() {
+    var count = 0;
+    var tempVal;
+    for (i = 0; i < 6; i++) {
+        if (document.getElementById("check"+i).checked == true) {
+            count++;
+            if (count > 1) {
+                alert("Please select only one restaurant.");
+                return false;
+            }
+            else {
+                tempVal = i;   
+            }
+        }
+    }
+    foods.splice(tempVal, 1);
+    displayFood();
+}
+
+
 
 $(document).ready(function() {
     $(".foodChoices").hide();
