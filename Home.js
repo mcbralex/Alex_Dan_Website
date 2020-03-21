d1b428c2b97791b10339615fe8efbc710864a808
Alex McBride <mcbralex@gmail.com>
2020-03-21 16:14:02 -0700

    lots

--- Home.js
+++ Home.js
@@ -1,12 +1,15 @@
 var foods = [];
+var user1 = "";
+var user2 = "";
+
 
 function listFood(){
     
-    var user1 = document.getElementById("user1").value;
+    user1 = document.getElementById("user1").value;
 
     document.getElementById("user1Choose").innerHTML = user1 + ", enter 3 restaurants.";
 
-    var user2 = document.getElementById("user2").value;
+    user2 = document.getElementById("user2").value;
 
     document.getElementById("user2Choose").innerHTML = user2 + ", enter 3 restaurants.";
 
@@ -27,16 +30,50 @@ function mixFood() {
 }
 
 function displayFood() {
-    for (i = 0; i < foods.length; i++){
-        document.getElementById("foods"+i).innerHTML = foods[i];
+    if (foods.length == 1){
+        winner();
     }
+    var linebreak = document.createElement("br");
+    var currentUser = document.createElement("label")
+
+    document.getElementById("checkList").innerHTML = "";
+
+    if (foods.length%2 != 0){
+        currentUser.innerHTML = user1+ ", remove 1 restaraunt";
+        document.getElementById("checkList").appendChild(currentUser);
+        document.getElementById("checkList").appendChild(linebreak);
+
+    }
+    else{
+        currentUser.innerHTML = user2+ ", remove 1 restaraunt";
+        document.getElementById("checkList").appendChild(currentUser);
+        document.getElementById("checkList").appendChild(linebreak);
+    }
+
+    for (i = 0; i < foods.length; i++){
+        var linebreak = document.createElement("br");
+
+        var newLabel = document.createElement("label");
+                    newLabel.id = "foodsLabel" + i;
+                    newLabel.innerHTML = foods[i];
+
+        var newInput = document.createElement("input");
+                    newInput.id = "checkFoods" + i;
+                    
+                    newInput.type = "checkbox";
+
+                    document.getElementById("checkList").appendChild(newInput);
+                    document.getElementById("checkList").appendChild(newLabel);                   
+                    document.getElementById("checkList").appendChild(linebreak);  
+    }
+
 }
 
 function subtractFood() {
     var count = 0;
     var tempVal;
-    for (i = 0; i < 6; i++) {
-        if (document.getElementById("check"+i).checked == true) {
+    for (i = 0; i < foods.length; i++) {
+        if (document.getElementById("checkFoods"+i).checked == true) {
             count++;
             if (count > 1) {
                 alert("Please select only one restaurant.");
@@ -52,13 +89,32 @@ function subtractFood() {
 }
 
 
+function winner(){
+    document.write("the winner is!!")
+}
+
+
+
 
 $(document).ready(function() {
     $(".foodChoices").hide();
+    $(".removeFood").hide();
+    $(".remove").hide();
 
     $("#userSubmit").click(function(){
         $(".users").hide();
+        
         $(".foodChoices").show();
+
+    })
+
+    $("#next").click(function(){
+        $(".foodChoices").hide();
+        $(".removeFood").show();
+
+        
+        
+        
 
     })
 });
